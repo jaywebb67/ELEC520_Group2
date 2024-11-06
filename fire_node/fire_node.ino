c842d1f55443f091cd201b47360ab34d798420a8
alexm2033 <alex.meredith@students.plymouth.ac.uk>
2024-11-06 21:04:07 +0000

    test code

--- fire_node/fire_node.ino
+++ fire_node/fire_node.ino
@@ -1,6 +1,6 @@
 #include "DHT.h"
+//#include <SoftwareSerial.h>
 #include <SoftwareSerial.h>
-
 #define DHTPIN 2     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 #define CE 4
@@ -12,6 +12,7 @@
 bool alarm_Pressed = false;
 String LED = "LED";
 
+int t = 4675;
 SoftwareSerial RS485Serial(10, 11);
 
 void alarm_Pressed_interrupt() {
@@ -35,33 +36,34 @@ void loop() {
   // Wait a few seconds between measurements.
   delay(2000);
 
-  if (RS485Serial.available()) {
-    //Serial.print("Hi");
-    // Read the received data
-    String receivedData = RS485Serial.readString();
-
-    //Serial.println(receivedData);
+  // if (RS485Serial.available()) {
+  //   //Serial.print("Hi");
+  //   // Read the received data
+  //   String receivedData = RS485Serial.readString();
+  //   //Serial.println(receivedData);
  
-    if(receivedData == LED) 
-      digitalWrite(LED_B, !digitalRead(LED_B));
-  }
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius
-  float t = dht.readTemperature();
+  //   if(receivedData == LED) 
+  //     digitalWrite(LED_B, !digitalRead(LED_B));
+  // }
+  // // Reading temperature or humidity takes about 250 milliseconds!
+  // // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // float h = dht.readHumidity();
+  // // Read temperature as Celsius
+  // // float t = dht.readTemperature();
+  // int t = 39765;
   
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
+  // // Check if any reads failed and exit early (to try again).
+  // if (isnan(h) || isnan(t)) {
+  //   Serial.println("Failed to read from DHT sensor!");
+  //   return;
+  // }
 
-  if(alarm_Pressed) {
-    Serial.println("Fire alarm triggered");
-  }
+  // if(alarm_Pressed) {
+  //   Serial.println("Fire alarm triggered");
+  // }
   
-  //Serial.print(t);
+  Serial.println(t);
+  Serial.println("I'm here");
   digitalWrite(CE, HIGH);
   RS485Serial.write(t);
   digitalWrite(CE, LOW);
