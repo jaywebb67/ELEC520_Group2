853d200da01ce9568e0830f0a9aa6b682bf77262
Alexander Fitzsimons <ajsfitzsimons4@gmail.com>
2024-12-03 10:40:01 +0000

    commit1

--- ConnectionCode/ConnectionCode.ino
+++ ConnectionCode/ConnectionCode.ino
@@ -1,6 +1,15 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerialRS485Serial(10,11);
+
+SoftwareSerial RS485Serial(10,11);
+
+unsigned char myAddress = 255;
+String myPurpose = "gate";
+char* message;
+
+const int whitePin = 4;
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -15,5 +24,32 @@ void loop() {
 
 void ConnectToNetwork(){
   //Serial.print("signal to adress 0");
-  RS485Serial.write("signal to adress 0")
+  //assembleMessage(myAddress, myPurpose);
+
+  RS485Serial.write("purpose and address");
+
+  while (myAddress == 255) {
+    digitalWrite(whitePin, HIGH); // sets the LED on
+    delay(500);                // waits for a second
+    digitalWrite(whitePin, LOW);  // sets the LED off
+    delay(500);
+    if (true){ // RS485Serial.available()
+      String receivedData = RS485Serial.readString();
+      message = undefinedFunction(receivedData);
+      Serial.println((unsigned int)(message[2]));
+      if ((unsigned int)message[2] == 65535) {
+        myAddress = 100;
+        digitalWrite(whitePin, HIGH); 
+        Serial.println("hello");
+      }
+    }
+
+  }
+
 }
+
+char* undefinedFunction(String data){
+  char msg[4] = {0, 0, 255, 0};
+  return msg;
+}
+
